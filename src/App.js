5ddf92aa3b75b017edd31029cab445db69980c2b
Alexander Shestak <110448322+MorugaShestak@users.noreply.github.com>
2022-08-28 12:56:40 +0400

    work on index page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Index from "./pages";
+
+function App() {
+  return (
+    <div className="App">
+
+      <Index/>
+
+    </div>
+  );
+}
+
+export default App;
